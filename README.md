8a91fb2dbe49ce4d46189353bafe2d562e9db6a9
Vendula Bezáková <vendula.bezakova@gmail.com>
2023-11-26 10:14:40 +0100

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript-cviceni-tridy
+
+![Výsledek](https://kodim.cz/cms/assets/vyvoj-webu/js1/lekce/dom-innerhtml/cv-tridy-innerhtml/nekupto-tridy/screen-nekupto.png)
---
+++ index.js
@@ -0,0 +1,27 @@
+const product1 = {
+  image: 'images/destniklobouk.jpg',
+  name: 'Deštníklobouk',
+  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+};
+
+const product2 = {
+  image: 'images/slepickabelka.jpg',
+  name: 'Slepičkabelka',
+  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+};
+
+const product3 = {
+  image: 'images/anatoplavky.jpg',
+  name: 'Anatoplavky',
+  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+};
+
+const mainProduct = document.querySelector("#product1");
+mainProduct.classList.add("border-primary");
+
+const button2 = document.querySelector('#product2 button');
+button2.classList.remove("btn-primary");
+
+const alignToggle = document.querySelector("#product3 .card-title");
+alignToggle.classList.toggle("text-center");
+
